c5b4c5427a6fd9829f726af31670de45e306fb92
VitoMarino <marino.vito2001@gmail.com>
2024-05-20 17:48:56 +0200

    Array bombe

--- readme.md
+++ readme.md
@@ -3,7 +3,16 @@ Copiamo la griglia fatta ieri nella nuova repo e aggiungiamo la logica del gioco
 
 Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
 Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+
 In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina.
+
 Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
 La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+
 Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+
+PSEUDOCODICE
+Ho 100 celle. In 16 celle voglio posizionare 1 bomba.
+
+Voglio che si generino 16 numeri randomici da 1 a 100. 
+Non voglio numeri randomici da 101 a salire.
--- script.js
+++ script.js
@@ -1,7 +1,47 @@
+// Variabili globali
 let divContainer = document.querySelector('div.container'); 
 let button = document.querySelector('button');
 let section = document.querySelector('section');
+let select = document.querySelector('#select-difficult');
+let article = document.querySelector('article')
 
+let arrayBombe = [];
+
+// FUNZIONI
+
+// Funzione randomica generale
+function numberRandomInt(min, max) {
+    return Math.floor(Math.random () * ((max + 1) - min)) + min;
+}
+
+for (let i = 1; i <= 16; i++) {
+    arrayBombe.push(numberUnicoRandom(arrayBombe, 1, 100));
+}
+
+// Funzione che ci permette di controllare SE un numero randomico, è stato già generato, e quindi deve essere "sostituito" da un altro randomico.
+function numberUnicoRandom(arrayBombe, min, max) {
+
+    let randomNumber;
+    let question = false;
+
+    while (!question) {
+        randomNumber= numberRandomInt(min, max);
+
+        // se il numero random generato NON è presente nella blacklist...
+        if (arrayBombe.includes(randomNumber) === false) {
+            // ... ho trovato il numero giusto. Esco dal ciclo.
+            question = true;
+        }
+    return randomNumber;
+}}
+
+if (arrayBombe === article) {
+    article.classList.add('red');
+}
+
+console.log(arrayBombe)
+
+// Bottone play
 button.addEventListener('click', function() {
     section.classList.add('active');
     divContainer.innerHTML = '';
@@ -10,7 +50,6 @@ button.addEventListener('click', function() {
         let article = document.createElement('article'); 
         divContainer.appendChild(article);
         article.append(i);
-    
         article.addEventListener('click', function() {
             article.classList.add('active');
             console.log(i);
@@ -20,5 +59,4 @@ button.addEventListener('click', function() {
         })
     }
 })
-
 
